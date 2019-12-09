a59516230af0f179d662fc55a4ea14adf184f6e9
PLUSLINKID <46049640+PLUSLINKID@users.noreply.github.com>
2019-12-10 01:07:24 +0800

    Update client.py

--- MyDFS/client.py
+++ MyDFS/client.py
@@ -76,95 +76,177 @@ class Client:
         
         # 新添内容
         # 对本地文件分块并发送，存储表的信息
-        def sort_sstable(sstable):
-            sstable.sort_values(by=['key'], inplace=True, ascending=True)
-            return sstable
-
-        # 一个sstable(data_pd)为64k，一行是不超过1k的数据,所以存储了64行数据(如果是100M,用100M/1k作为行数)
-        # 一行里面放行键值和内容
-        # 生成sstable并进行编码,key目前是文件名
+        def get_content(little_file_path):
+            file_type = os.path.splitext(little_file_path)[1]
+            if file_type == '.txt':
+                file_content = open(little_file_path).read()
+            elif file_type == '.png':
+                file_ = open(little_file_path, 'rb')
+                file_content = str(base64.b64encode(file_.read()))
+                file_.close()
+            else:
+                print('识别不出数据类型的数据集')
+                file_content=None
+            return file_content
         
-        # tablet(ss_pd)存放：1、sstable的索引 2、sstable的地址 3、sstable中的最后一行行键值，
+        def sort_encode(sort_table):
+            sort_table.sort_values(by=['key'], inplace=True, ascending=True)
+            return sort_table
+        
+        def encode_file(file_path):
+            return os.path.abspath(file_path)
+        
+        # 一个sstable为64k，一行是不超过1k的数据,所以存储了64行数据(如果是100M,用100M/1k作为行数)
+        # 前63行每行里面放行键值和内容,最后一行是下一个sstable的地址
+        # 生成sstable,key目前是文件名
+        
+        # tablet存放：1、sstable的索引 2、sstable的地址 3、sstable中的最后一行行键值，
         # 当tablet存放到4k截止
         # 默认一行内容不超过100Bytes,可以存40行
         
-        # root meta(tablet_pd)存储1、tablet的索引 2、tablet的地址 3、tablet中的最后一行行键值，
+        # root meta存储1、tablet的索引 2、tablet的地址 3、tablet中的最后一行行键值，
         
-        data_pd = pd.DataFrame(columns=['key', 'content'])
-        ss_pd = pd.DataFrame(columns=['ss_index', 'host_name', 'last_key'])
-        tablet_pd = pd.DataFrame(columns=['tablet_index', 'host_name','last_key'])
+        sstable = pd.DataFrame(columns=['key', 'content'])
+        tablet = pd.DataFrame(columns=['ss_index', 'host_name', 'last_key'])
+        root = pd.DataFrame(columns=['tablet_index', 'host_name','last_key'])
+        host_name_tr = host_list[np.random.choice(len(host_list), dfs_replication, replace=False)]
 
-        pd_index = 0
-        sstable_index = 0
-        tablet_index = 0
+        # 对所有文件的编码进行排序（运行时间比较长，所以先存下来了）
+        # #已保存，之后如果有新的数据集或到新机器上重新运行一次
+        # sort_table=pd.DataFrame(columns=['key'])
+        # sort_index=0
+        # for type_file in os.listdir(local_path):
+        #     for file in os.listdir(local_path + type_file):
+        #         for little_file in os.listdir(local_path + type_file + '/' + file):
+        #             little_file_path = local_path + type_file + '/' + file + '/' + little_file
+        #             sort_table.loc[sort_index]=encode_file(little_file_path)
+        #             sort_index+=1
+        # file_index=sort_encode(sort_table)
+        # file_index.to_csv('sort_table.csv')
+        
+        file_sort=pd.read_csv('sort_table.csv')
+        #   所有文件的长度
+        all_len=len(file_sort)
+        #   选择63是因为最后一行是指向下一个sstable的地址
+        sstable_num=int(all_len/63)
+        data_end_num=all_len-63*sstable_num
+        ss_total_num=sstable_num+ bool(data_end_num/63)
+        
+        tablet_num=int(ss_total_num/40)         #存满的tablet_num个数
+        tablet_rest=ss_total_num-tablet_num*40  #最后一个tablet中存放的sstable的个数
+        tablet_total_num=tablet_num+bool(tablet_rest)
+        
+        flag=True
 
-        for type_file in os.listdir(local_path):
-            for file in os.listdir(local_path + type_file):
-                for little_file in os.listdir(local_path + type_file + '/' + file):
-                    little_file_path = local_path + type_file + '/' + file + '/' + little_file
-                    file_type = os.path.splitext(little_file_path)[1]
-                    if file_type == '.txt':
-                        file_content = open(little_file_path).read()
-                    elif file_type == '.png':
-                        file_ = open(little_file_path, 'rb')
-                        file_content = str(base64.b64encode(file_.read()))
-                        file_.close()
-                    else:
-                        print('识别不出数据类型的数据集')
-                    data_pd.loc[pd_index] = [str(little_file_path), file_content]
-                    pd_index += 1
-                    if pd_index == 64:
-                        # 一个sstable已经生成，要排序并发送出去，同时作为一行存储在tablet中。
-                        sstable=sort_sstable(data_pd)
-                        #选择三个副本存放的地址
-                        host_name = host_list[np.random.choice(len(host_list), dfs_replication, replace=False)]
-                        #往三个地址上发送data
-                        blk_path = dfs_path + ".sstable{}".format(40*tablet_index+sstable_index)
-                        #精准的大小值
-                        sstable.to_csv('test_size.csv',index=False)
-                        length_data=os.path.getsize('test_size.csv')
-                        fp=open('test_size.csv')
-                        print(self.sendToHosts(host_name, blk_path, length_data, fp))
-                        fp.close()
-                        
-                        #往tablet表中增加一行
-                        last_key=sstable['key'][len(sstable)-1]
-                        ss_pd.loc[sstable_index] = [sstable_index,host_name, last_key]
+        for tabindex in range (tablet_total_num):
+            tablet = pd.DataFrame(columns=['ss_index', 'host_name', 'last_key'])
+            if not flag:
+                break
+            for ssindex in range (40):
+                sstable = pd.DataFrame(columns=['key', 'content'])
+                if not flag:
+                    break
+                for data_index in range (63):
+                    #往sstable里填充内容
+                    file_index=40*63*tabindex+63*ssindex+data_index
+                    little_file_path = file_sort['key'][file_index]
+                    if not os.path.exists(little_file_path):
+                        flag=False
+                        break
+                    file_content=get_content(little_file_path)
+                    sstable.loc[data_index]=[str(little_file_path),file_content]
+                
+                #如果有上一个sstable，往上一个的最后一行存放当前sstable的地址，并将上一个sstable传出去1
+                if 'host_name' in locals().keys()  and 'blk_path' in locals().keys() :
+                    host_name0=host_name
+                    blk_path0=blk_path
+                
+                #选择当前sstable的地址
+                host_name = host_list[np.random.choice(len(host_list), dfs_replication, replace=False)]
+                blk_path = dfs_path + ".sstable{}.csv".format(40*tabindex+ssindex)
+    
+                # 存储到tablet
+                last_key=sstable['key'][len(sstable)-1]
+                tablet.loc[ssindex] = [ssindex,host_name,last_key]
+                # 如果有上一个sstable，往上一个的最后一行存放当前sstable的地址，并将上一个sstable传出去2
+                if 'host_name0' in locals().keys() and 'blk_path0' in locals().keys():
+                    send_sstable = pd.read_csv('ss.csv')
+                    send_sstable.loc[len(send_sstable)] = [ssindex, host_name]
+                    send_sstable.to_csv('ss.csv', index=False)
+                    length_data = os.path.getsize('ss.csv')
+                    fp = open('ss.csv')
+                    print(self.sendToHosts(host_name0, blk_path0, length_data, fp))
+                    fp.close()
 
-                        #重新开启下一个sstable的存储
-                        pd_index = 0
-                        sstable_index += 1
-                        
-                    if sstable_index == 40:
-                        #一个tablet满了，要发出去
-                        ##存到root_meta_table中（存放tablet的地址）
-                        host_name = host_list[np.random.choice(len(host_list), dfs_replication, replace=False)]
-                        blk_path = dfs_path + ".tablet{}".format(tablet_index)
-                        
-                        ss_pd.to_csv('test_size.csv',index=False)
-                        length_data=os.path.getsize('test_size.csv')
-                        fp1=open('test_size.csv')
-                        
-                        #发送到各个服务器上
-                        print(self.sendToHosts(host_name, blk_path, length_data, fp1))
-                        fp1.close()
-                        last_key=ss_pd['last_key'][len(ss_pd)-1]
-                        tablet_pd.loc[tablet_index] = [tablet_index, host_name,last_key]
-                        sstable_index = 0
-                        tablet_index += 1
-                        
-        #按照FAT表发送root_meta_tablet表
-        for idx, row in fat.iterrows():
-            # 解析host地址
-            tablet_pd.to_csv('test_size.csv',index=False)
-            length_data = os.path.getsize('test_size.csv')
-            fp2=open('test_size.csv')
-            host_str = row['host_name']
-            host_names = get_hosts(host_str)
-            blk_path = dfs_path + ".root_tablet{}".format(row['blk_no'])
-            print(self.sendToHosts(host_names, blk_path, length_data, fp2))
+                # 将当前的sstable存放到ss.csv中(一个临时文件，不断被覆盖的)
+                sstable.to_csv('ss.csv', index=False)
+        
+            # 当tablet存满到40行,发出去并存储到root
+            blk_path_tablet = dfs_path + ".tablet{}.csv".format(tabindex)
+            tablet.to_csv('tablet.csv',index=False)
+            length_data=os.path.getsize('tablet.csv')
+            fp_t=open('tablet.csv')
+            print(self.sendToHosts(host_name_tr, blk_path_tablet, length_data, fp_t))
+            root.loc[tabindex] = [tabindex,host_name_tr,last_key]
+        
+        #*到这里最后一个sstable还没有发送出去
+        #如果正好是整数个满的sstable表，在最后一个sstable最后增加None
+        if data_end_num==0:
+            last_sstable = pd.read_csv('ss.csv')
+            #增加一行index,None
+            last_sstable.loc[len(last_sstable)] = [sstable_num, 'None']
+            last_sstable.to_csv('ss.csv', index=False)
+            length_data = os.path.getsize('ss.csv')
+            fp1=open('ss.csv')
+            print(self.sendToHosts(host_name, blk_path, length_data, fp1))
+            fp1.close()
+            
+        # 如果不是整数个满的sstable表，在最后一个sstable增加新表的地址并发出，之后发送存储满的sstable
+        # 还要在tablet之后再新加一行
+        else:
+            #读剩下来的sstable内容
+            for rest_data_index in range (data_end_num):
+                file_index=63*sstable_num+rest_data_index
+                little_file_path = file_sort['key'][file_index]
+                file_content=get_content(little_file_path)
+                sstable.loc[rest_data_index]=[str(little_file_path),file_content]
+                
+            #安排剩下的sstable存放的地址
+            host_name_re = host_list[np.random.choice(len(host_list), dfs_replication, replace=False)]
+            blk_path_re = dfs_path + ".sstable{}.csv".format(ssindex)
+            
+            #增加一行,host_name_re，并发送
+            last_sstable = pd.read_csv('ss.csv')
+            last_sstable.loc[len(last_sstable)] = [sstable_num, host_name_re]
+            last_sstable.to_csv('ss.csv', index=False)
+            length_data = os.path.getsize('ss.csv')
+            fp2=open('ss.csv')
+            print(self.sendToHosts(host_name, blk_path, length_data, fp2))
             fp2.close()
             
+            #在最后的sstable后面增加一行None ,发送最后的sstable
+            last_key=sstable['key'][len(sstable)-1]
+            sstable['key'][len(sstable)]=['end','None']
+            
+            sstable.to_csv('ss.csv', index=False)
+            length_data = os.path.getsize('ss.csv')
+            fp3=open('ss.csv')
+            print(self.sendToHosts(host_name_re, blk_path_re, length_data, fp3))
+            fp3.close()
+            
+            # 将最后的sstable存入到tablet表中
+            tablet.loc[len(tablet)] = [sstable_num,host_name_re,last_key]
+            root.loc[len(root)]=[tablet_total_num,'aitat',last_key]
+            #发送最后的root和tablet
+            tablet.to_csv('tabletend.csv',index=False)
+            root.to_csv('root.csv',index=False)
+            blk_path_tr = dfs_path + ".tablet{}.csv".format(tablet_total_num)
+            blk_path_fr = dfs_path + ".root{}.csv".format(0)
+            fpt=open('tabletend.csv')
+            fpr=open('root.csv')
+            print(self.sendToHosts(host_name_tr, blk_path_tr, length_data, fpt))
+            print(self.sendToHosts(host_name_tr, blk_path_fr, length_data, fpr))
+
     def sendToHosts(self, host_names, blk_path, length_data, fp):
         host_str = str(host_names).replace(' ', '')
         for host in host_names:
