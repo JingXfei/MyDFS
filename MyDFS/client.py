628ada9d76430fbfac70499705e497210bb9b21b
Mac <mac@MacBook-Pro-5.local>
2019-12-10 09:13:47 +0800

    add query

--- MyDFS/client.py
+++ MyDFS/client.py
@@ -198,15 +198,8 @@ class Client:
         return "fail"
     
     def copyToLocal(self, dfs_path, local_path):
-        request = "get_fat_item {}".format(dfs_path)
-        print("Request: {}".format(request))
-        
-        # 从NameNode获取一张FAT表
-        self.name_node_sock.send(bytes(request, encoding='utf-8'))
-        fat_pd = self.name_node_sock.recv(BUF_SIZE)
-        
-        # 打印FAT表，并使用pandas读取
-        fat_pd = str(fat_pd, encoding='utf-8')
+        # 调用getFatItem函数得到Fat表；
+        fat_pd = getFatItem(dfs_path)
         print("Fat: \n{}".format(fat_pd))
         fat = pd.read_csv(StringIO(fat_pd))
         
@@ -312,6 +305,243 @@ class Client:
         result = str(result, encoding='utf-8')
         print("result: \n{}".format(result))
 
+    def queryArea(self, dfs_path, local_path_1, local_path_2 = "None"):
+        # 范围查询；
+        # local_path_2不为None时说明范围是[local_path_1, local_path_2];
+        # 否则是查询local_path_1目录下的值；
+        # 这里可以进行处理，比如查询目录时，查到哪里结束也是需要以row_key的形式进行表述的。
+        # 首先反复调用query_table查到第一个；再基于叶节点之间的链式索引关系进行后续遍历。
+        row_key = rowkey_encode(local_path_1)
+        if local_path_2 != "None":
+            row_key_2 = rowkey_encode(local_path_2)
+        else:
+            row_key_2 = "None"
+        path = []
+
+        # 调用getFatItem函数得到Fat表；并处理fat表不存在的情况
+        fat = getFatItem(dfs_path)
+        if fat == "None":
+            print("No this Fat!")
+            return None
+        print("Next table: \n{}".format(fat))
+        fat = pd.read_csv(StringIO(fat))
+        path.append(fat)
+
+        # 三层查询位置
+        root_tablet = queryTable(fat, 0, dfs_path, row_key)
+        root_tablet = str(root_tablet, encoding='utf-8')
+        if root_tablet == "None":
+            print("Bigger than Biggest, area not in this BigTable")
+            return None
+        print("Next table: \n{}".format(root_tablet))
+        root_tablet = pd.read_csv(StringIO(root_tablet))
+        path.append(root_tablet)
+
+        # 经过上述过程，得知row_key必定可以存在于该树的结构中(即可以添加到某个叶节点中)
+        # ——>查询tablet——>查询sstable:
+        tablet = queryTable(root_tablet, 1, dfs_path, row_key) # 得到tablet中对应的一行；
+        tablet = str(tablet, encoding='utf-8')
+        print("Next table: \n{}".format(tablet))
+        tablet = pd.read_csv(StringIO(tablet))
+        path.append(tablet)
+
+        while True:
+            res,nex = queryTableArea(tablet, dfs_path, row_key, row_key_2) # 得到sstable中一个区域的内容；
+            print("Result: \n{}".format(res)) # 一次只打印一个sstable的，再将该空间重新利用
+            nex = pd.read_csv(StringIO(nex))
+            if nex.shape[0] == 0:
+                break
+            else:
+                tablet = pd.DataFrame(columns=['ss_index', 'host_name'])
+                tablet.iloc[0] = [int(nex.iloc[0]['key'][len("sstable"):]), nex.iloc[0]['content']]
+
+    def queryOne(self, dfs_path, local_path, bloom_filter = True):
+        # 这个是一个完整的、单独用于单点查询的函数，范围查询另写。
+        # 返回值有三种情况：
+        # 1. None：bloomFilter校验失败，大于存储最大lastkey
+        # 2.[
+        #    [blk_no, host_name, blk_size], Fat表的一行；
+        #    [tablet_index, host_name, last_key], rootTablet的一行；
+        #    [ss_index, host_name, last_key], tablet的一行；
+        #    [key, content] 最终的内容和行键
+        #   ] # 若查询成功，则返回对应row_key的，否则返回大于该row_key的第一个的。
+
+        row_key = rowkey_encode(local_path)
+        path = []
+
+        # 调用getFatItem函数得到Fat表；并处理fat表不存在的情况
+        fat = getFatItem(dfs_path)
+        if fat == "None":
+            print("No this Fat!")
+            return path
+        print("Next table: \n{}".format(fat))
+        fat = pd.read_csv(StringIO(fat))
+        path.append(fat)
+
+        if bloom_filter:
+            # 先进行bloomFilter，
+            # 若校验失败即文件不存在，返回None；
+            res_filter = bloomFilter(fat, dfs_path, row_key)
+            res_filter = str(res_filter, encoding='utf-8')
+            if res_filter == "None":
+                print("Not exist in this BigTable")
+                return path
+
+        # 若校验成功，分情况讨论：
+        #   row_key小于整棵树最大的last_key，则返回None；
+        #   否则返回rootTablet中对应的Tablet所在一行。
+        root_tablet = queryTable(fat, 0, dfs_path, row_key) # 得到root_tablet中对应的一行；
+        root_tablet = str(root_tablet, encoding='utf-8')
+        if root_tablet == "None":
+            print("Bigger than Biggest, area not in this BigTable")
+            return path
+        print("Next table: \n{}".format(root_tablet))
+        root_tablet = pd.read_csv(StringIO(root_tablet))
+        path.append(root_tablet)
+
+        # 经过上述过程，得知row_key必定可以存在于该树的结构中(即可以添加到某个叶节点中)
+        # ——>查询tablet——>查询sstable:
+        tablet = queryTable(root_tablet, 1, dfs_path, row_key) # 得到tablet中对应的一行；
+        tablet = str(tablet, encoding='utf-8')
+        print("Next table: \n{}".format(tablet))
+        tablet = pd.read_csv(StringIO(tablet))
+        path.append(tablet)
+
+        sstable = queryTable(tablet, 2, dfs_path, row_key) # 得到sstable中对应的一行；
+        content = str(sstable, encoding='utf-8')
+        print("Content: \n{}".format(content))
+        sstable = pd.read_csv(StringIO(sstable))
+        path.append(sstable)
+
+        print(path)
+        return path
+
+    def bloomFilter(self, fat, dfs_path, row_key):
+        # 依据要查询的row_key生成两个index，传入bloomfilter所在位置进行校验；
+        index_1 = hash1(row_key)
+        index_2 = hash2(row_key)
+
+        end = '.bloom_filter'
+
+        row = fat.iloc[0]
+        host_str = row['host_name']
+        host_names = get_hosts(host_str)
+        for host in host_names:
+            try:
+                time.sleep(0.2)
+                data_node_sock = socket.socket()
+                print("bloomFilter from "+host+"...",end='')
+                data_node_sock.connect((host, data_node_port))
+                table_path = dfs_path + end
+                # 传输查询的table的路径、在B+树中的层数、行键
+                request = "bloomFilter {} {} {}".format(table_path, index_1, index_2)
+                data_node_sock.send(bytes(request, encoding='utf-8'))
+                time.sleep(0.2)  # 两次传输需要间隔一段时间，避免粘包
+                res = data_node_sock.recv(BUF_SIZE)
+                data_node_sock.close()
+                break
+            except:
+                data_node_sock.close()
+                print(host+" error!")
+                if host == host_names[-1]:
+                    return "All Error!"
+                continue
+        return res
+
+    def queryTable(self, cur_table, cur_layer, dfs_path, row_key):
+        if cur_layer == 0:
+            end = ".root_tablet"
+            ind_col = "blk_no"
+        elif cur_layer == 1:
+            end = ".tablet"
+            ind_col = "tablet_index"
+        else:
+            end = ".sstable"
+            ind_col = "ss_index"
+
+        row = cur_table.iloc[0]
+        host_str = row['host_name']
+        host_names = get_hosts(host_str)
+        for host in host_names:
+            try:
+                time.sleep(0.2)
+                data_node_sock = socket.socket()
+                print("get table from "+host+"...",end='')
+                data_node_sock.connect((host, data_node_port))
+                table_path = dfs_path + end + "{}".format(row[ind_col])
+                # 传输查询的table的路径、在B+树中的层数、行键
+                request = "query {} {} {}".format(table_path, cur_layer, row_key)
+                data_node_sock.send(bytes(request, encoding='utf-8'))
+                time.sleep(0.2)  # 两次传输需要间隔一段时间，避免粘包
+                table = data_node_sock.recv(BUF_SIZE)
+                data_node_sock.close()
+                break
+            except:
+                data_node_sock.close()
+                print(host+" error!")
+                if host == host_names[-1]:
+                    return "All Error!"
+                continue
+        return table
+
+    def queryTableArea(self, cur_table, dfs_path, row_key_begin, row_key_end = "None"):
+        # 对指定的sstable（包含范围第一个row_key)（及后续），获取范围
+        # 如果end为"None"，则在datanode中按照ls的逻辑进行查询；否则找begin和end之间的数据
+        res = ""
+        row = cur_table.iloc[0]
+        host_str = row['host_name']
+        host_names = get_hosts(host_str)
+        for host in host_names:
+            try:
+                time.sleep(0.2)
+                data_node_sock = socket.socket()
+                print("get table from "+host+"...",end='')
+                data_node_sock.connect((host, data_node_port))
+                table_path = dfs_path + ".sstable{}".format(row['ss_index'])
+                # 传输查询的table的路径、开始行键、结束行键
+                request = "queryArea {} {} {}".format(table_path, row_key_begin, row_key_end)
+                data_node_sock.send(bytes(request, encoding='utf-8'))
+                length_data = int(data_node_sock.recv(BUF_SIZE))
+                # 接收数据
+                ################################
+                size_rec = 0
+                while size_rec < length_data:
+                    data = data_node_sock.recv(BUF_SIZE)
+                    size_rec = size_rec + len(data)
+                    data = str(data, encoding='utf-8')
+                    res = res+data
+                ################################
+                next_sstable = data_node_sock.recv(BUF_SIZE)
+                next_sstable = str(next_sstable, encoding='utf-8')
+                data_node_sock.close()
+                break
+            except:
+                data_node_sock.close()
+                print(host+" error!")
+                if host == host_names[-1]:
+                    return "All Error!"
+                continue
+        return (res,next_sstable)
+
+    def insert(self, local_path, dfs_path):
+        # 此处应该调用行键编码函数进行编码 #################################################
+        row_key = rowkey_encode(local_path) # 相对路径？绝对路径？
+        insert_path = self.queryOne(dfs_path, local_path, bloom_filter = False)
+        
+
+    def getFatItem(self, dfs_path):
+        request = "get_fat_item {}".format(dfs_path)
+        print("Request: {}".format(request))
+        
+        # 从NameNode获取一张FAT表
+        self.name_node_sock.send(bytes(request, encoding='utf-8'))
+        fat_pd = self.name_node_sock.recv(BUF_SIZE)
+        
+        # 打印FAT表，并使用pandas读取
+        fat_pd = str(fat_pd, encoding='utf-8')
+
+        return fat_pd
+
 # 解析命令行参数并执行对于的命令
 argv = sys.argv
 argc = len(argv) - 1
@@ -355,13 +585,42 @@ elif cmd == "-mapReduce":
         client.mapReduce(map_file, reduce_file, dfs_path)
     else:
         print("Usage: python client.py -mapReduce <map_file> <reduce_file> <dfs_path>")
-elif cmd=="-sscopyFromLocal":
+elif cmd == "-sscopyFromLocal":
     if argc == 3:
         local_path = argv[2]
         dfs_path = argv[3]
         client.sscopyFromLocal(local_path, dfs_path)
     else:
         print("Usage: python client.py -copyFromLocal <local_path> <dfs_path>")
+elif cmd == "-query":
+    if argc == 3:
+        dfs_path = argv[2]
+        local_path = argv[3]
+        client.queryOne(dfs_path, local_path)
+    else:
+        print("Usage: python client.py -query <dfs_path> <filename_str>")
+elif cmd == "-queryArea":
+    if argc == 4:
+        dfs_path = argv[2]
+        local_path_1 = argv[3]
+        local_path_2 = argv[4]
+        client.queryOne(dfs_path, local_path_1, local_path_2)
+    else:
+        print("Usage: python client.py -queryArea <dfs_path> <local_path_begin> <local_path_end>")
+elif cmd == "-ssls":
+    if argc == 3:
+        dfs_path = argv[2]
+        local_path = argv[3]
+        client.queryArea(dfs_path, local_path)
+    else:
+        print("Usage: python client.py -ssls <dfs_path> <local_path>")
+elif cmd == "-insert":
+    if argc == 2:
+        local_path = argv[2]
+        dfs_path = argv[3]
+        client.insert(local_path, dfs_path)
+    else:
+        print("Usage: python client.py -insert <local_path> <dfs_path>")
 else:
     print("Undefined command: {}".format(cmd))
     print("Usage: python client.py <-ls | -copyFromLocal | -copyToLocal | -rm | -format> other_arguments")
